3e39da50bbf5794e8957b8c2ab56aa4034db492a
farzad-bahadorifar <farzad.bahadorifar@gmail.com>
2023-10-11 00:12:53 +0330

    feat: Implement form submission and routing logic
    Import ReactiveFormsModule and HttpClientModule dependencies
    Configure login component to use reactive forms
    Define form control validators and group
    Add login submission handler
    Call authentication service on submit
    Handle responses and UI updates

--- src/app/account/login.component.ts
+++ src/app/account/login.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AccountService } from '@app/_services';
+import { first } from 'rxjs';
 
 @Component({ templateUrl: 'login.component.html' })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
   form!: FormGroup;
   loading = false;
   submitted = false;
@@ -15,5 +16,47 @@ export class LoginComponent {
     private route: ActivatedRoute,
     private router: Router,
     private accountService: AccountService
-  ) {}
+  ) {
+    if (this.accountService.userValue) {
+        this.router.navigate(['/'])
+    }
+  }
+    ngOnInit(): void {
+        this.form = this.formBuilder.group({
+            username: ['', Validators.required],
+            password: ['',Validators.required]
+        })
+    }
+
+    get f() {
+        return this.form.controls;
+    }
+    
+
+    onSubmit() {
+        this.submitted = true;
+
+        this.error = '';
+
+        if (this.form.invalid) {
+            return;
+        }
+
+        this.loading = true;
+
+        this.accountService.login(this.f.username.value, this.f.password.value)
+        .pipe(first())
+        .subscribe({
+            next: () => {
+                const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+                this.router.navigateByUrl(returnUrl);
+            },
+            error: error => {
+                this.error = error;
+                this.loading = false;
+            }
+        })
+    }
+
+  
 }
