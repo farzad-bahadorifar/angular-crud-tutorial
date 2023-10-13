0dc58ace2cfd0507eb72cb37481919509fa0fb41
farzad-bahadorifar <farzad.bahadorifar@gmail.com>
2023-10-13 22:01:19 +0330

    feat: Add logout functionality and toggle navbar
    Import AccountService dependency to AppComponent
    Subscribe to user observable and set property
    Add logout method calling service method
    Update navbar template
    Conditionally show based on user
    Add Home and Logout links
    Wire up logout button click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AccountService } from './_services';
+import { User } from './_models';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-crud-tutorial';
+  user?: User | null;
+
+  constructor(private accountService: AccountService) {
+    this.accountService.user.subscribe(x => this.user = x)
+  }
+
+  logout() {
+    this.accountService.logout();
+  }
 }
