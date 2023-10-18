d4e5db46505842a9f2a95f879cc53381cdaf347c
farzad-bahadorifar <farzad.bahadorifar@gmail.com>
2023-10-18 23:49:35 +0330

    feat: Add form logic to registration component
    Import required dependencies into RegisterComponent
    Check for existing user and redirect in constructor
    Define form group with controls in ngOnInit hook
    Add f() getter for shorthand access to controls
    Implement onSubmit() method for handling submission
    Call registration service on submit with form values
    Handle responses and display loading/error states

--- src/app/account/register.component.ts
+++ src/app/account/register.component.ts
@@ -1,4 +1,60 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+import { AccountService } from "@app/_services";
 
 @Component({templateUrl: 'register.component.html'})
-export class RegisterComponent {}
+export class RegisterComponent implements OnInit {
+
+    form!: FormGroup;
+    loading = false;
+    submitted = false;
+    error?: string;
+
+    
+    constructor(
+        private formBuilder: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router,
+        private accountService: AccountService
+    ) {
+        if (this.accountService.userValue) {
+            this.router.navigate(['/'])
+        }
+    }
+
+    ngOnInit(): void {
+        this.form = this.formBuilder.group({
+            firstName: ['', Validators.required],
+            lastName: ['', Validators.required],
+            username: ['', Validators.required],
+            password: ['', [Validators.required, Validators.minLength(6)]]
+        });
+    }
+
+    get f() {return this.form.controls;}
+
+    onSubmit() {
+        this.submitted = true;
+
+        this.error = '';
+
+        if (this.form.invalid) {
+            return;
+        }
+
+        this.loading = true;
+        this.accountService.register(this.form.value)
+        .pipe(first())
+        .subscribe({
+            next: () => {
+                this.router.navigate(['/account/login']), {queryParams: {registered: true}}
+            }, 
+            error: error => {
+                this.error = error;
+                this.loading = false;
+            }
+        });
+    }
+
+}
